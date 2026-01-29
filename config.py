022fc4892b04f1d794418eb740cd522647bd0172
ioxAbhi <avtaryadavchungi@gmail.com>
2026-01-29 11:40:18 +0530

    Update upstream repository and support channel URLs

--- config.py
+++ config.py
@@ -37,14 +37,14 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/viscity/Abhimusic.git",
+    "https://github.com/ioxabhi/Abhimusic.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Thetiamusicbots")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BotGravedyard")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Lczyf4wpVGVmNDI1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
